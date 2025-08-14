a5fff5e6fcea5ccde819373b738e096ba2d5c970
Christopher Dodge <cdodge14@wgu.edu>
2025-08-13 21:04:19 -0700

    D479: changing vite base

--- taniti-prototype/vite.config.js
+++ taniti-prototype/vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ]
+    base: '/d479-user-experience-prototype/',
+    plugins: [
+        react(),
+        tailwindcss()
+    ]
 })
